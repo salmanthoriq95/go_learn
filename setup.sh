21d1c83147f311665763bac78c4c8218f575fba5
salmanthoriq95 <salmanthoriq95@gmail.com>
2022-08-04 13:09:45 +0700

    add auto config bash script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# Setup GOPATH and PATH env variables. use source command or just single dot
+export GOPATH=${PWD};
+export PATH=$PATH:${PWD}/bin;
+echo "GOPATH and PATH variables has been changed!"
