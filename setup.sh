f092047cc372c928e2e6ac863c0c12cd3f1306a5
salmanthoriq95 <salmanthoriq95@gmail.com>
2022-08-04 13:20:09 +0700

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# Setup GOPATH and PATH env variables. use source command or just single dot
+# Setup GOPATH and PATH env variables. use source command or just single dot, example: . setup.sh
 export GOPATH=${PWD};
 export PATH=$PATH:${PWD}/bin;
 echo "GOPATH and PATH variables has been changed!"
